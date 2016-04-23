fbc87cb31f70298b7740ee9be564b602a3f0b703
Gleb Glazov <glazov.gleb@gmail.com>
2016-04-23 18:20:26 +0300

    Renaming config files and making install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+install_dotfiles () {
+    local overwrite_all=true backup_all=true skip_all=false
+
+    for src in $(find "$DIR/" -maxdepth 2 -name '*.src')
+    do
+        dst="$HOME/$(basename "${src%.*}")"
+        ln -s "$src" "$dst"
+    done
+}
+
+install_dotfiles
