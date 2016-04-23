10b2854c6a34f377125abcc4ed998a41889df8a4
Gleb Glazov <glazov.gleb@gmail.com>
2016-04-23 21:22:04 +0300

    [FIX] Remove unused local variables

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 install_dotfiles () {
-    local overwrite_all=true backup_all=true skip_all=false
-
     for src in $(find "$DIR/" -maxdepth 2 -name '*.src')
     do
         dst="$HOME/$(basename "${src%.*}")"
