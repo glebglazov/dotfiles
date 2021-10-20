ad4fc3863f316882b7951934fc437b898c761aa1
Gleb Glazov <glazov.gleb@gmail.com>
2021-10-20 21:01:20 +0000

    Add iptables configuration for Mosh as well as general cleanup

--- workstation_bootstrap.sh
+++ workstation_bootstrap.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y software-properties-common ripgrep git
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
+# Install some basic packages
+
+apt-get install -y software-properties-common ripgrep git mosh tmux
+apt-get update
+
+# Setup iptables
+
+iptables_rule="INPUT -p udp --dport 60000:61000 -j ACCEPT"
+iptables -C $iptables_rule || sudo iptables -A $iptables_rule
+
+iptables_config_dir="/etc/iptables"
+mkdir -p $iptables_config_dir
+
+iptables_config_path="$iptables_config_dir/rules.v4"
+[ -f $iptables_config_path ] || iptables-save > $iptables_config_path
+
+# Git setup
 
 git config --global user.name "Gleb Glazov"
 git config --global user.email "glazov.gleb@gmail.com"
+
