a42535513050b91ea73cc0b806f8b0bcbfeb1329
Gleb Glazov <glazov.gleb@gmail.com>
2021-10-21 20:38:34 +0000

    Add .my-profile and install tmux

--- workstation_bootstrap.sh
+++ workstation_bootstrap.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
 # Install some basic packages
-
 apt-get install -y software-properties-common ripgrep git mosh tmux
 apt-get update
 
-# Setup iptables
+# Install tmux
+apt-get install tmux
 
+# Configure bash a bit
+source_dot_my_profile_text=". $HOME/.my-profile"
+(cat $HOME/.bashrc | grep "$source_dot_my_profile_text") || echo "$source_dot_my_profile_text" >> $HOME/.bashrc
+cp .my-profile $HOME/.my-profile
+. $HOME/.bashrc
+
+# Setup iptables
 iptables_rule="INPUT -p udp --dport 60000:61000 -j ACCEPT"
 iptables -C $iptables_rule || sudo iptables -A $iptables_rule
 
@@ -17,7 +24,5 @@ iptables_config_path="$iptables_config_dir/rules.v4"
 [ -f $iptables_config_path ] || iptables-save > $iptables_config_path
 
 # Git setup
-
 git config --global user.name "Gleb Glazov"
 git config --global user.email "glazov.gleb@gmail.com"
-
