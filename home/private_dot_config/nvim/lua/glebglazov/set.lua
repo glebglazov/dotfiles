90bff1d84ece929c56d3a4ec14d722851c95bfb2
Gleb Glazov <glazov.gleb@gmail.com>
2022-12-29 21:02:01 +0100

    Set up undotree plugin even more

--- home/private_dot_config/nvim/lua/glebglazov/set.lua
+++ home/private_dot_config/nvim/lua/glebglazov/set.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = ' '
 
 vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.opt.undofile = true
 
 vim.opt.expandtab=true
 vim.opt.tabstop=2
