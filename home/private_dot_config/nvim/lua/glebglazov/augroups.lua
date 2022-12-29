dbe3d9a59dc1970304b01ca09469ab5846de590c
Gleb Glazov <glazov.gleb@gmail.com>
2022-12-29 19:19:41 +0100

    Defaulting vim at 2 spaces identing

--- home/private_dot_config/nvim/lua/glebglazov/augroups.lua
+++ home/private_dot_config/nvim/lua/glebglazov/augroups.lua
@@ -12,35 +12,7 @@ ShellGroup = augroup('Shell', {})
 autocmd({ 'FileType' }, {
 	group = ShellGroup,
 	pattern = 'sh',
-	command = 'setlocal expandtab tabstop=4 shiftwidth=4',
-})
-
-RubyGroup = augroup('Ruby', {})
-autocmd({ 'FileType' }, {
-	group = RubyGroup,
-	pattern = 'ruby',
-	command = 'setlocal expandtab tabstop=2 shiftwidth=2',
-})
-
-JsGroup = augroup('JS', {})
-autocmd({ 'FileType' }, {
-	group = JsGroup,
-	pattern = 'javascript',
-	command = 'setlocal expandtab tabstop=2 shiftwidth=2',
-})
-
-ReactGroup = augroup('React', {})
-autocmd({ 'FileType' }, {
-	group = JsGroup,
-	pattern = 'javascriptreact',
-	command = 'setlocal expandtab tabstop=2 shiftwidth=2',
-})
-
-LuaGroup = augroup('Lua', {})
-autocmd({ 'FileType' }, {
-	group = LuaGroup,
-	pattern = 'lua',
-	command = 'setlocal expandtab tabstop=2 shiftwidth=2',
+	command = 'setlocal expandtab tabstop=4 softtabstop=4 shiftwidth=4',
 })
 
 FugitiveGroup = augroup('Fugitive', {})
--- home/private_dot_config/nvim/lua/glebglazov/set.lua
+++ home/private_dot_config/nvim/lua/glebglazov/set.lua
@@ -2,6 +2,11 @@ vim.g.mapleader = ' '
 
 vim.opt.swapfile = false
 
+vim.opt.expandtab=true
+vim.opt.tabstop=2
+vim.opt.softtabstop=2
+vim.opt.shiftwidth=2
+
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
