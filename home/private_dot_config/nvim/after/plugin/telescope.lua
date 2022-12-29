b3f84f42b4523b4f25cb400ef09a64d347024795
Gleb Glazov <glazov.gleb@gmail.com>
2022-12-29 23:08:30 +0100

    Configure telescope resume on <LEADER>?

--- home/private_dot_config/nvim/after/plugin/telescope.lua
+++ home/private_dot_config/nvim/after/plugin/telescope.lua
@@ -6,14 +6,14 @@ local get_visual_selection = require('glebglazov.functions.get_visual_selection'
 local nnoremap  = require('glebglazov.functions.remap').nnoremap
 local vnoremap  = require('glebglazov.functions.remap').vnoremap
 
-nnoremap('<LEADER>bb', function() telescope.buffers() end, { silent = true })
-nnoremap('<LEADER>/', function() telescope.live_grep() end, { silent = true })
+nnoremap('<LEADER>bb', telescope.buffers)
+nnoremap('<LEADER>/', telescope.live_grep)
+nnoremap('<LEADER>?', telescope.resume)
 vnoremap('<LEADER>/', function() telescope.grep_string({ search = get_visual_selection() }) end, { silent = true })
 
 nnoremap('<LEADER>pf', function()
   telescope.find_files({
     hidden = true,
-
     file_ignore_patterns = { "^./.git/", "^node_modules/" },
   })
 end)
