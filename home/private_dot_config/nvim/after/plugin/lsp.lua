ecad643068f1022471e168bccb427eb44ffdafe9
Gleb Glazov <glazov.gleb@gmail.com>
2023-01-12 10:57:04 +0100

    Use telescope LSP references in Nvim

--- home/private_dot_config/nvim/after/plugin/lsp.lua
+++ home/private_dot_config/nvim/after/plugin/lsp.lua
@@ -1,4 +1,6 @@
 local lsp = require('lsp-zero')
+local telescope = require('telescope.builtin')
+local nnoremap  = require('glebglazov.functions.remap').nnoremap
 
 lsp.preset('recommended')
 lsp.set_preferences({
@@ -8,8 +10,8 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+  nnoremap('gd', function() vim.lsp.buf.definition() end, opts)
+  nnoremap('gr', telescope.lsp_references, opts)
 end)
 
 lsp.setup()
