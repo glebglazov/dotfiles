57c69bd31f9b70e8fdd89984cd585eeac888b53a
Gleb Glazov <glazov.gleb@gmail.com>
2023-03-11 11:35:22 +0100

    Fixing terraform flow with templates of SSH keys

--- home/exact_dot_zshrc_includes/20_aliases.sh
+++ home/exact_dot_zshrc_includes/20_aliases.sh
@@ -1,8 +1,8 @@
 alias vim="nvim"
-alias ide="tmux-ide-layout.sh"
+alias ide="tmux-ide-layout"
 
 alias aws="op run -- aws"
 alias bundle="op run -- bundle"
 alias kubectl="op run -- kubectl"
-alias terraform="op run -- terraform"
+alias terraform="op run -- my_terraform"
 alias yarn="op run -- yarn"
