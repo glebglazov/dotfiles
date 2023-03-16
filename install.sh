edd2b6b4abf991ba2262a06e7d5debf1d2bc01f7
Gleb Glazov <glazov.gleb@gmail.com>
2023-03-16 23:06:13 +0100

    Update submodules in ./install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,8 @@ else
   chezmoi=chezmoi
 fi
 
+git submodule update --init
+
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
