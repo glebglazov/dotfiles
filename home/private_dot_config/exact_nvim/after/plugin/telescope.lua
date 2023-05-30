0bb5f4fc2efc1242d079a59193967e228fc46245
Gleb Glazov <glazov.gleb@gmail.com>
2023-05-30 17:19:03 +0200

    telescope.lua: add <leader>fb and <leader>fh bindings

--- home/private_dot_config/exact_nvim/after/plugin/telescope.lua
+++ home/private_dot_config/exact_nvim/after/plugin/telescope.lua
@@ -7,10 +7,12 @@ local get_visual_selection = require('glebglazov.functions.get_visual_selection'
 local nnoremap  = require('glebglazov.functions.remap').nnoremap
 local vnoremap  = require('glebglazov.functions.remap').vnoremap
 
-nnoremap('<LEADER>bb', telescope.buffers)
 nnoremap('<LEADER>/', telescope.live_grep)
 nnoremap('<LEADER>?', telescope.resume)
 vnoremap('<LEADER>/', function() telescope.grep_string({ search = get_visual_selection() }) end, { silent = true })
+
+nnoremap('<LEADER>fb', telescope.buffers)
+nnoremap('<LEADER>fh', telescope.help_tags)
 
 nnoremap('<LEADER>pf', function()
   telescope.find_files({
