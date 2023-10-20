9ee5f6a7359251c03a52e236c2bdc989b164bb7c
Gleb Glazov <glazov.gleb@gmail.com>
2023-10-20 12:26:00 +0200

    abc

--- home/private_dot_config/exact_nvim/after/exact_plugin/nvim-tree.lua
+++ home/private_dot_config/exact_nvim/after/exact_plugin/nvim-tree.lua
@@ -24,3 +24,5 @@ require('nvim-tree').setup({
 nnoremap('<LEADER>fl', '<CMD>NvimTreeOpen<CR>', { silent = true })
 nnoremap('<LEADER>ff', '<CMD>NvimTreeFocus<CR>', { silent = true })
 nnoremap('<LEADER>fc', '<CMD>NvimTreeClose<CR>', { silent = true })
+
+somechange
