50c8060b5dd8816dd1399903223e6e6e50ef86e3
Gleb Glazov <glazov.gleb@gmail.com>
2023-11-21 18:45:22 +0100

    lsp: fix ruby lsp cause now lint are not reloaded after fixing violations

--- home/private_dot_config/exact_nvim/after/exact_plugin/lsp.lua
+++ home/private_dot_config/exact_nvim/after/exact_plugin/lsp.lua
@@ -35,39 +35,52 @@ configure.yamlls.setup({
   }
 })
 
+-- textDocument/diagnostic support until 0.10.0 is released
+_timers = {}
+local function setup_diagnostics(client, buffer)
+  if require("vim.lsp.diagnostic")._enable then
+    return
+  end
+
+  local diagnostic_handler = function()
+    local params = vim.lsp.util.make_text_document_params(buffer)
+    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+      if err then
+        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
+        vim.lsp.log.error(err_msg)
+      end
+      local diagnostic_items = {}
+      if result then
+        diagnostic_items = result.items
+      end
+      vim.lsp.diagnostic.on_publish_diagnostics(
+        nil,
+        vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
+        { client_id = client.id }
+      )
+    end)
+  end
+
+  diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+  vim.api.nvim_buf_attach(buffer, false, {
+    on_lines = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+    end,
+    on_detach = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+    end,
+  })
+end
+
 configure.ruby_ls.setup({
   on_attach = function(client, buffer)
-    local diagnostic_handler = function ()
-      local params = vim.lsp.util.make_text_document_params(buffer)
-      client.request(
-        'textDocument/diagnostic',
-        { textDocument = params } ,
-        function(err, result)
-          if err then
-            local err_msg = string.format("ruby-lsp - diagnostics error - %s", vim.inspect(err))
-            vim.lsp.log.error(err_msg)
-          end
-          if not result then return end
-          vim.lsp.diagnostic.on_publish_diagnostics(
-            nil,
-            vim.tbl_extend('keep', params, { diagnostics = result.items }),
-            { client_id = client.id }
-          )
-        end
-      )
-    end
-
-    diagnostic_handler() -- to request diagnostics when attaching the client to the buffer
-
-    local ruby_group = vim.api.nvim_create_augroup('ruby_ls', {clear = false})
-    vim.api.nvim_create_autocmd(
-      { 'BufEnter', 'BufWritePre', 'InsertLeave', 'TextChanged' },
-      {
-        buffer = buffer,
-        callback = diagnostic_handler,
-        group = ruby_group,
-      }
-    )
+    setup_diagnostics(client, buffer)
   end
 })
 
