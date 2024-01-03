17c70855812f9e182f928394c3839918813be75e
Gleb Glazov <glazov.gleb@gmail.com>
2024-01-03 23:34:36 +0000

    removing sh overrides from nvim config

--- home/private_dot_config/exact_nvim/init.lua
+++ home/private_dot_config/exact_nvim/init.lua
@@ -638,21 +638,6 @@ autocmd({ 'FileType' }, {
 })
 
 -------------------------------------------------
--- Autogroups — Sh
--------------------------------------------------
-local sh_keybindings = augroup('sh_keybindings', {})
-autocmd({ 'FileType' }, {
-	group = sh_keybindings,
-	pattern = 'sh',
-	callback = function ()
-    vim.opt_local.expandtab = true
-    vim.opt_local.tabstop = 4
-    vim.opt_local.softtabstop = 4
-    vim.opt_local.shiftwidth = 4
-  end
-})
-
--------------------------------------------------
 -- Keybindings
 -------------------------------------------------
 
