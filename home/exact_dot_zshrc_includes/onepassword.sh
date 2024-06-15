56a280d491dcdf081feb282a06cadae3a961a26a
Gleb Glazov <glazov.gleb@gmail.com>
2024-06-15 15:12:37 +0000

    fix op signin script

--- home/exact_dot_zshrc_includes/onepassword.sh
+++ home/exact_dot_zshrc_includes/onepassword.sh
@@ -1,7 +1,10 @@
 op-signin() {
   account_shorthand=$1
-  token=$(op signin --account $account_id --raw)
-  account_id=$(op account get | grep "ID" | awk '{print $2}')
+  user_id=$(op account list | grep $account_shorthand | awk '{print $4}')
 
+  token=$(op signin --account $user_id --raw)
+  export "OP_SESSION_$user_id"=$token
+
+  account_id=$(op account get | grep "ID" | awk '{print $2}')
   export "OP_SESSION_$account_id"=$token
 }
