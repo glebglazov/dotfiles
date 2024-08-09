b01570d8a19c6adb416f2d31b9816ba038de830b
Gleb Glazov <glazov.gleb@gmail.com>
2024-08-09 17:06:31 +0200

    add gruvbox for init-tools

--- home/private_dot_config/exact_nvim/init-tools.lua
+++ home/private_dot_config/exact_nvim/init-tools.lua
@@ -1,3 +1,43 @@
 vim.opt.number = true
-
+vim.opt.updatetime = 250
+vim.opt.background = 'dark'
 vim.opt.iskeyword:remove('_')
+
+-------------------------------------------------
+-- Initialise lazy.nvim
+-------------------------------------------------
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-------------------------------------------------
+-- Plugins setup
+-------------------------------------------------
+require('lazy').setup({
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    version = '2.0.0',
+    config = function ()
+      local gruvbox = require('gruvbox')
+
+      gruvbox.setup({
+        overrides = {
+          DiffAdd = { fg = "#b8bb26", bg = "NONE" },
+          DiffDelete = { fg = "#fb4934", bg = "NONE" },
+        },
+      })
+
+      vim.cmd([[colorscheme gruvbox]])
+    end
+  }
+})
