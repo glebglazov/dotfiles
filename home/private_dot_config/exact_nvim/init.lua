995175d19015ca551510d09fc6cdf395cd6bf753
Gleb Glazov <glazov.gleb@gmail.com>
2024-10-31 17:05:20 +0100

    install copilot chat for neovim

--- home/private_dot_config/exact_nvim/init.lua
+++ home/private_dot_config/exact_nvim/init.lua
@@ -198,6 +198,20 @@ require('lazy').setup({
       panel = { enabled = false },
     }
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
   { 'zbirenbaum/copilot-cmp', config = true },
   { 'tpope/vim-sleuth' }, -- Heuristic tabstop / softtabstop / etc.
 
