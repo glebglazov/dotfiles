78d7c5f881cc419c23bc8d003e6ec1913646dcb2
Gleb Glazov <glazov.gleb@gmail.com>
2025-04-26 00:18:14 +0200

    remove some crazy LSP mappings

--- home/private_dot_config/exact_nvim/init.lua
+++ home/private_dot_config/exact_nvim/init.lua
@@ -756,16 +756,6 @@ lsp_zero.on_attach(function(_, buffnr)
   vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', {buffer = buffnr})
   vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', {buffer = buffnr})
   vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', {buffer = buffnr})
-  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', {buffer = buffnr})
-  vim.keymap.set('n', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', {buffer = buffnr})
-  vim.keymap.set('x', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', {buffer = buffnr})
-  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', {buffer = buffnr})
-
-  if vim.lsp.buf.range_code_action then
-    vim.keymap.set('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>', {buffer = buffnr})
-  else
-    vim.keymap.set('x', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', {buffer = buffnr})
-  end
 
   vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', {buffer = buffnr})
   vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', {buffer = buffnr})
