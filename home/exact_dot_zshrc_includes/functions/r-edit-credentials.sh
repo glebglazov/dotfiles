3b5a402c4d9e0ba3b5dd6c01da4c041cc628e144
Gleb Glazov <glazov.gleb@gmail.com>
2025-08-11 12:19:51 +0200

    start rails edit credentials in interactive zsh shell

--- home/exact_dot_zshrc_includes/functions/r-edit-credentials.sh
+++ home/exact_dot_zshrc_includes/functions/r-edit-credentials.sh
@@ -1,5 +1,5 @@
 function r-edit-credentials {
     local env_name="$1"
 
-    RAILS_MASTER_KEY="op://$OP_RAILS_MASTER_KEY_BASE/$env_name" op run -- _be_wrap rails credentials:edit --environment "$env_name"
+    RAILS_MASTER_KEY="op://$OP_RAILS_MASTER_KEY_BASE/$env_name" op run -- zsh -ic "_be_wrap rails credentials:edit --environment '$env_name'"
 }
