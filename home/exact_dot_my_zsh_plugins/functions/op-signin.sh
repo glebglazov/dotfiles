10ac68e59c27554c4730d78fb8bd4fd83466d33c
Gleb Glazov <glazov.gleb@gmail.com>
2026-04-01 23:41:36 +0000

    export only one env var from op-signin

--- home/exact_dot_my_zsh_plugins/functions/op-signin.sh
+++ home/exact_dot_my_zsh_plugins/functions/op-signin.sh
@@ -2,10 +2,5 @@ function op-signin {
     local account_shorthand="${1:-personal}"
     local token=$(op signin --account "$account_shorthand" --raw)
 
-    local account_uuid=$(op account list --format=json | jq -r ".[] | select(.shorthand == \"$account_shorthand\") | .account_uuid")
-    local user_uuid=$(op account list --format=json | jq -r ".[] | select(.shorthand == \"$account_shorthand\") | .user_uuid")
-
     export "OP_SESSION_$account_shorthand"="$token"
-    export "OP_SESSION_$account_uuid"="$token"
-    export "OP_SESSION_$user_uuid"="$token"
 }
